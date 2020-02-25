03817a478b4255e5609f1321cc99dfcad197a56b
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-02-24 20:51:07 -0600

    Duplicate using explicit recursion

--- src/2. Repetition/problemset1.clj
+++ src/2. Repetition/problemset1.clj
@@ -9,6 +9,17 @@
     *'
     (range 1 (inc n))))
 
+(defn duplicate
+  "Duplicates each element in lst."
+  [lst]
+  (if (empty? lst)
+    ()
+    (cons
+      (first lst)
+      (cons
+        (first lst)
+        (duplicate (rest lst))))))
+
 (deftest test-!
   (is (= 1
          (! 0)))
@@ -20,5 +31,15 @@
          (! 25)))
   (is (= 815915283247897734345611269596115894272000000000N
          (! 40))))
+
+(deftest test-duplicate
+  (is (= '(1 1 2 2 3 3 4 4 5 5)
+         (duplicate '(1 2 3 4 5))))
+  (is (= ()
+         (duplicate ())))
+  (is (= '(a a)
+         (duplicate '(a))))
+  (is (= '(a a b b c c d d e e f f g g h h)
+         (duplicate '(a b c d e f g h)))))
 
 (run-tests)
