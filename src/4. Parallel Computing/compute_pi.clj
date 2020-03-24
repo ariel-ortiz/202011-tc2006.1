7779a340f92f7295187bf8474e45dde8a466b24e
John Camaney <mathogebut@matra.top>
2020-03-23 22:26:47 -0600

    Using pmap to parallelize your code

--- src/4. Parallel Computing/compute_pi.clj
+++ src/4. Parallel Computing/compute_pi.clj
@@ -1,14 +1,23 @@
 (defn compute-pi
   "Approximates the value of Pi using the numerical integration midpoint
   algorithm."
-  [num-rects]
+  [num-rects start end]
   (let [width (/ 1.0 num-rects)]
-    (loop [i 0
+    (loop [i start
            sum 0.0]
       (let [mid (* (+ i 0.5) width)
             height (/ 4.0 (+ 1.0 (* mid mid)))]
-        (if (= i num-rects)
+        (if (= i end)
           (* width sum)
           (recur
             (inc i)
             (+ sum height)))))))
+
+(defn parallel-pi
+  [num-rects]
+  (let [mid (/ num-rects 2)]
+    (->>
+      (list [num-rects 0 mid] [num-rects mid num-rects])
+      (pmap (fn [[num-rects start end]]
+              (compute-pi num-rects start end)))
+      (reduce +))))
