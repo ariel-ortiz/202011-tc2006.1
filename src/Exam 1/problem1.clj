0c6694314c55ff9d6a437688b6399fc2a3ff2f38
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-04-13 15:12:18 -0500

    Base source file for the first exam

---
+++ src/Exam 1/problem1.clj
@@ -0,0 +1,29 @@
+;==========================================================
+; Type your student ID and name here.
+;==========================================================
+
+(require '[clojure.test :refer [deftest is run-tests]])
+
+;==========================================================
+(defn lucky-number?
+  "Returns true if n is a positive integer number that is
+  exactly divisible by 7 or contains at least one digit 7.
+  Otherwise returns false."
+  [n]
+  nil)
+
+;==========================================================
+(deftest test-lucky-number?
+ (is (lucky-number? 7))
+ (is (lucky-number? 2828))
+ (is (lucky-number? 773704))
+ (is (lucky-number? 0))
+ (is (lucky-number? 14890123808))
+ (is (lucky-number? 100000700000))
+ (is (not (lucky-number? 1)))
+ (is (not (lucky-number? 26)))
+ (is (not (lucky-number? 123456890)))
+ (is (not (lucky-number? 14890123806))))
+
+;==========================================================
+(run-tests)
