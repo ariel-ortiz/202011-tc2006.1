dff9e1231dc784b3f48ae822f36519d422bbfd0c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-05-18 20:48:51 -0500

    Class exercises bif and def-vars

--- src/7. Macros/examples.clj
+++ src/7. Macros/examples.clj
@@ -32,3 +32,35 @@
         (my-and ~@next)
         temp#))))
 
+(defmacro bif
+  "Binds the condition to a variable.
+  If that value is true, evaluates and
+  returns de then-part, otherwise
+  evaluates and returns the else-part."
+  [variable condition then-part else-part]
+  `(let [~variable ~condition]
+     (if ~variable
+       ~then-part
+       ~else-part)))
+
+(defn def-vars-helper
+  "Creates individual (def var-name-suffix expr)
+  lists."
+  [var-name expressions]
+  (map (fn [n expr]
+         `(def ~(symbol (str var-name n)) ~expr))
+       (range)
+       expressions))
+
+(defmacro def-vars
+  "Defines as many global variables as the provided
+  number of expressions. The value of var-name is the
+  name prefix for all these variables. The suffix
+  is “0” for the first variable name, which is
+  initialized with the first expression. The next
+  variable name has a “1” suffix and is initialized
+  with the second expression, and so on with all the
+  remaining variables."
+  [var-name & expressions]
+  `(do ~@(def-vars-helper var-name expressions)))
+
