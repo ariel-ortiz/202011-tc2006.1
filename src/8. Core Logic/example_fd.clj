f29cfa216710495a29b3e12838310b2b5fa9e003
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-06-02 12:01:08 -0500

    Solution to the magic square problem

--- src/8. Core Logic/example_fd.clj
+++ src/8. Core Logic/example_fd.clj
@@ -25,3 +25,21 @@
   ([[head . tail] temp]
    (largesto tail temp)
    (fd/> temp head)))
+
+(defn magic-square
+  "Solves the 3x3 magic square puzzle."
+  []
+  (logic/run*
+    [q1 q2 q3
+     q4 q5 q6
+     q7 q8 q9]
+    (fd/in q1 q2 q3 q4 q5 q6 q7 q8 q9 (fd/interval 1 9))
+    (fd/distinct [q1 q2 q3 q4 q5 q6 q7 q8 q9])
+    (sumo [q1 q2 q3] 15)
+    (sumo [q4 q5 q6] 15)
+    (sumo [q7 q8 q9] 15)
+    (sumo [q1 q4 q7] 15)
+    (sumo [q2 q5 q8] 15)
+    (sumo [q3 q6 q9] 15)
+    (sumo [q1 q5 q9] 15)
+    (sumo [q7 q5 q3] 15)))
